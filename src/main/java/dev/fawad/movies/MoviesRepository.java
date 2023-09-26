088ca37a9941aac8b4897eccf70f87bb689852fb
Fawad Iqbal <122690338+fawadeqbal@users.noreply.github.com>
2023-09-27 00:11:58 +0500

    init

---
+++ src/main/java/dev/fawad/movies/MoviesRepository.java
@@ -0,0 +1,9 @@
+package dev.fawad.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MoviesRepository extends MongoRepository<Movie, ObjectId>{
+}

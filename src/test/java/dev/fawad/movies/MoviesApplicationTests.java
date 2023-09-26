c47d09ddc12e341e749cc480355593657867738b
Fawad Iqbal <122690338+fawadeqbal@users.noreply.github.com>
2023-09-26 22:42:48 +0500

    init

---
+++ src/test/java/dev/fawad/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fawad.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
